2da37cd8592a081618542b0f0016f50646511ed4
shlokmehrotra <shlokmehrotra@gmail.com>
2017-12-26 21:43:51 -0800

    Server js from fixed c9 workspace

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
 var express = require('express');
-var mysql = require('mysql');
-var port = process.env.PORT;
-var path = require('path')
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'altruisticnarcissist',
-    password: '',
-    database: 'c9'
-});
+
 var app = express();
-/*app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(app.router);
-});
-*/
-app.set('views', path.join(__dirname, 'main_files'))
-//app.use(express.static(path.resolve('./main_files/')));
-//app.use(express.static('main_files'));
-//app.use('/main_files');
+var server = http.createServer(app);
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+
 app.get('/', function(req, res){
-    res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, '/index.html')); 
 });
-app.listen(port);
+
+app.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname, '/client/about.html')); 
+  //res.sendFile('about');
+});
+
+app.get('/contact', function(req, res){
+  res.sendFile(path.join(__dirname, '/client/contact.html')); 
+});
+
+app.get('/password', function(req, res){
+  res.sendFile(path.join(__dirname, '/client/password.html')); 
+});
+app.get('/membership', function(req, res){
+  res.sendFile(path.join(__dirname, '/client/membership.html'))
+})
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  console.log("Server listening...");
+});
